37bf6b2a4d3f8d985f88fab2918f248296fce812
Valentin Venzhega <valek917@yandex.ru>
2021-02-01 23:55:19 +0300

    удалил данные из урока 2

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-let num = 266219;
-num = String(num).split('').reduce((sum, current) => sum * current);
-console.log(num);
-
-num = num**3;
-console.log(num);
-
-num = +String(num).slice(0, 2);
-// alert(num);
-console.log(num);
-
-
-
 // lesson03
 const lang = 'en';
 const rusLang = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
