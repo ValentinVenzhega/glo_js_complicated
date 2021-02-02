684b36107f9ea9f57dd8890bf2ea4b37104566f2
Valentin Venzhega <valek917@yandex.ru>
2021-02-03 00:05:55 +0300

    усложненный урок 4 выполнил

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
 'use strict';
 
-// lesson03
-const lang = 'en';
-const rusLang = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
-const engLang = 'monday, tuesday, wednesday, thursday, friday, saturday, sunday';
 
-
-if (lang === 'ru') {
-   console.log(rusLang);
-}else if (lang === 'en') {
-   console.log(engLang);
+function foo(name) {
+   if (name.length < 30) {
+      console.log(name.trim());
+   } else if(name.length >= 30) {
+      console.log(name.slice(0 , 29) + "...");
+   } else {
+      console.log('не строка');
+   }
 }
 
-
-switch(lang) {
-   case 'ru': 
-      console.log(rusLang);
-      break;
-   case  'en':
-      console.log(engLang);
-      break;
-}
-
-
-const arr = [
-   ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'], 
-   ['monday, tuesday, wednesday, thursday, friday, saturday, sunday']
-];
-
-const week = (lang === 'en') ? arr[1] : arr[0];
-console.log(week);
-
-
-const namePerson =  prompt('Введите имя').toLowerCase();
-const message = (namePerson === 'артем') ? 'Директор' :
-   (namePerson === 'максим') ? 'Преподаватель' :
-   'Студент';
-
-console.log(message);
+foo(5);
