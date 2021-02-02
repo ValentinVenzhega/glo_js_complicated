e0412b3a9fbc66b317b456fa00a5345ebad240ed
Valentin Venzhega <valek917@yandex.ru>
2021-02-03 00:10:21 +0300

    добавил метод trim во втором условии

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function foo(name) {
    if (name.length < 30) {
       console.log(name.trim());
    } else if(name.length >= 30) {
-      console.log(name.slice(0 , 29) + "...");
+      console.log(name.slice(0 , 29).trim() + "...");
    } else {
       console.log('не строка');
    }
 }
 
-foo(5);
+foo('     ssscscsc    ');
