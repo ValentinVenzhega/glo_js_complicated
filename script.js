ee7e93d8f8ceb82c7bf50babfb026518c2bba5b0
Valentin Venzhega <valek917@yandex.ru>
2021-02-05 23:07:48 +0300

    lesson07

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 'use strict';
 
-const arr = ['26', '63', '45', '99', '32', '24', '12'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+   itemList = document.querySelectorAll('li');
 
-for (let i = 0; i < arr.length; i++){
-   if(arr[i][0] === '2' || arr[i][0] === '4') {
-
-      console.log(arr[i]);
-   }
-}
+   week.forEach(item => {
+      console.log(item.li);
+   });
 
 
-nextPrime:
-for (let i = 2; i <= 100; i++) { // Для всех i...
 
-  for (let j = 2; j < Math.sqrt(i); j++) { // проверить, делится ли число..
-      if (i % j === 0) {
-         continue nextPrime; // не подходит, берём следующее
-      } 
-   }
 
-   console.log(`${i} делится на 1 и на ${i}`);
-}
+// const day = document.getElementById('fordays'); // Получаем div куда всё будем вставлять
+// const todayDay = new Date();
+
+// const days = () => {
+//     week.forEach((item, i) => {
+//         let newdiv = document.createElement('div'); // Для каждого элемента week создаём div
+//         if (i === +todayDay.getDay()-1) { // Если текущий день недели то от номера текущей даты отнимаем единицу, так как массив у нас начинается с нуля
+//             console.log(todayDay.getDay());
+//             newdiv.classList.add('today'); // Добавляем класс (делаем жирным)
+//             newdiv.textContent = week[i]; // Вставляем текст
+//         }
+//         if (item == 'Суббота' || item == 'Воскресенье') { // Если выходные то
+//             newdiv.classList.add('italic'); // Делаем дополнительно курсивом (Даже если уже жирным выделено)
+//             newdiv.textContent = week[i]; // Вставляем текст
+//         } else {
+//             newdiv.textContent = week[i]; // Если не текущий день и не выходные, то просто вставляем текст без стилей
+//         }
+//         day.appendChild(newdiv); // Добавляем новый див в div @day
+
+//     });
+// };
+// days(); // Вызываем функцию;
 
