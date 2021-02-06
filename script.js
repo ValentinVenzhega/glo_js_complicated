001159d8a79fb639caa4433e1eb7fe4026e0df4d
Valentin Venzhega <valek917@yandex.ru>
2021-02-06 14:17:37 +0300

    исправление 4

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 'use strict';
 
+// const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//    dayCurrent = document.querySelector('.days'),
+//    date = new Date(),
+//    days = () => {
+//    week.forEach((item, i) => {
+//       let div = document.createElement('div');
+//       if (i === date.getDay() - 3) { // -1 так как массив
+//          if (item === 'Суббота' || item === 'Воскресенье') {
+//             div.style.fontStyle = 'italic'; 
+//          }
+//          div.style.fontWeight = '600';
+//       }
+//       if ((item === 'Суббота' || item === 'Воскресенье')) { 
+//          div.style.fontStyle = 'italic'; 
+//          div.textContent = week[i];
+//       }  else {
+//          div.textContent = week[i];
+//       }
+//       dayCurrent.append(div); 
+//    });
+// };
+// days();
+
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
    dayCurrent = document.querySelector('.days'),
    date = new Date(),
    days = () => {
    week.forEach((item, i) => {
       let div = document.createElement('div');
-      if (i === date.getDay() - 3) { // -1 так как массив
+      if (i === date.getDay() - 1) { // -1 так как массив
          if (item === 'Суббота' || item === 'Воскресенье') {
             div.style.fontStyle = 'italic'; 
          }
