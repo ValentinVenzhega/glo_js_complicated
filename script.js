b1dbe92c4ce00dee8e42c4d37441ebc1f03bc0b9
Valentin Venzhega <valek917@yandex.ru>
2021-02-06 17:10:05 +0300

    испрвил колличество символов

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function foo(name) {
    if (typeof(name) === 'string' && name.trim().length < 30) {
       console.log(name.trim());
    } else if(typeof(name) === 'string' && name.trim().length > 30) {
-      console.log(name.trim().slice(0 , 29) + '...' );
+      console.log(name.trim().slice(0 , 30) + '...' );
    } else {
       console.log('не строка');
    }
 }
 
 foo('  ss  ');
-foo('                                                        s                                       ');
+foo('                                                        12345678901234567890123456789012                                       ');
 foo(5);
 
