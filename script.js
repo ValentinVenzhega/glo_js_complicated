0ce2c1f2ca18053c95c14b5dd9ebef17a1ce5455
Valentin Venzhega <valek917@yandex.ru>
2021-02-06 15:12:54 +0300

    сделал отчистку строки до сравнения

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 
 function foo(name) {
-   if (typeof(name) === 'string' && name.length < 30 && name.trim()) {
+   if (typeof(name) === 'string' && name.trim().length < 30 && name.trim()) {
       console.log(name.trim());
-   } else if(typeof(name) === 'string' && name.length >= 30 && name.trim()) {
+   } else if(typeof(name) === 'string' && name.trim().length >= 30 && name.trim()) {
       console.log(name.trim().slice(0 , 29) + '...' );
    } else {
       console.log('не строка');
