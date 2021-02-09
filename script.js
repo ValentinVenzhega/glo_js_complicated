1da447fe137866d361faa02e09f5af986b5d4f09
Valentin Venzhega <valek917@yandex.ru>
2021-02-10 00:42:43 +0300

    задание 9 доп выполнено

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 'use strict';
+const dateOne = document.querySelector('.date-one'),
+	dateTwo = document.querySelector('.date-two');
 
 
-function foo(name) {
-   if (typeof(name) === 'string' && name.trim().length < 30) {
-      console.log(name.trim());
-   } else if(typeof(name) === 'string' && name.trim().length > 30) {
-      console.log(name.trim().slice(0 , 30) + '...' );
-   } else {
-      console.log('не строка');
-   }
+function addZero(num) {
+	if (num >= 0 && num <= 9) {
+		return '0' + num;
+	} else {
+		return num;
+	}
+}
+function declOfNum(number, words) {  
+	return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
 }
 
-foo('  ss  ');
-foo('                                                        12345678901234567890123456789012                                       ');
-foo(5);
+setInterval(function(){
+	let date = new Date(),
+		days = ["воскресенье", "понедельник","вторник","среда","четверг","пятница","суббота"],
+		month = ["января", "февраля","марта","апреля","мая","июня","июля", "августа", "сентября", "октября", "ноября", "декабря"];
 
+	dateOne.innerHTML = `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${addZero(date.getFullYear())} - ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
+	dateTwo.innerHTML = `Сегодня ${days[date.getDay()]} ${date.getDay()} ${month[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} ${declOfNum(date.getHours(), [ 'день', 'дня', 'дней' ])}  ${date.getMinutes()} ${declOfNum(date.getMinutes(), [ 'минута', 'минуты', 'минут' ])} ${date.getSeconds()} ${declOfNum(date.getSeconds(), [ 'секунда', 'секунды', 'секунд' ])}`;
+}, 1000);
