92b6d7016a750ddbcafa03c041c10afceb5f6bf0
Valentin Venzhega <valek917@yandex.ru>
2021-02-10 23:31:50 +0300

    задание 9 выполнено

--- script.js
+++ script.js
@@ -2,32 +2,33 @@
 const dateOne = document.querySelector('.date-one'),
 	dateTwo = document.querySelector('.date-two');
 
-function addZero(num) {
-	if (num >= 0 && num <= 9) {
-		return '0' + num;
-	} else {
-		return num;
-	}
-}
-
 function declOfNum(number, words) {  
 	return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
 }
 
 setInterval(function(){
-	let date = new Date(),
-      options = {
-      month: 'long',
-      day: 'numeric',
-      weekday: 'long',
-      timezone: 'UTC'
-      };
+	const date = new Date(),
+		options = {
+			month: 'long',
+			day: 'numeric',
+			weekday: 'long',
+			timezone: 'UTC'
+		},
+		month = date.toLocaleString("ru", options).split(', ').map(word => word[0].toUpperCase() + word.substring(1)).join(' '),
+		year = date.getFullYear(),
+		hour = date.getHours(),
+		minute = date.getMinutes(),
+		second = date.getSeconds(),
+		hourText = declOfNum(date.getHours(), [ 'час', 'часа', 'часов' ]),
+		minuteText = declOfNum(date.getMinutes(), [ 'минута', 'минуты', 'минут' ]),
+		secondText = declOfNum(date.getSeconds(), [ 'секунда', 'секунды', 'секунд' ]);
 
-	dateOne.innerHTML = `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}
-                        .${addZero(date.getFullYear())} - ${addZero(date.getHours())}
-                        :${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
-	dateTwo.innerHTML = `Сегодня ${date.toLocaleString("ru", options)} ${date.getFullYear()} года, 
-                        ${date.getHours()} ${declOfNum(date.getHours(), [ 'час', 'часа', 'часов' ])}  ${date.getMinutes()} 
-                        ${declOfNum(date.getMinutes(), [ 'минута', 'минуты', 'минут' ])} ${date.getSeconds()} 
-                        ${declOfNum(date.getSeconds(), [ 'секунда', 'секунды', 'секунд' ])}`;
+      console.log(options);
+
+
+	dateOne.innerHTML = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+	dateTwo.innerHTML = `Сегодня ${month} ${year} года, 
+      ${hour} ${hourText}  ${minute} 
+      ${minuteText} ${second} 
+      ${secondText}`;
 }, 1000);
