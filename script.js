2e752f044f9b0d96e7024e92552b629da025e404
Valentin Venzhega <valek917@yandex.ru>
2021-02-17 21:34:36 +0300

    задание 13 выполнено

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
+
 'use strict';
-const dateOne = document.querySelector('.date-one'),
-	dateTwo = document.querySelector('.date-two');
 
-function declOfNum(number, words) {  
-	return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
-}
+const btn = document.querySelector('.btn'),
+   colorText = document.querySelector('.color');
 
-setInterval(function(){
-	const date = new Date(),
-		options = {
-			month: 'long',
-			day: 'numeric',
-			weekday: 'long',
-			timezone: 'UTC'
-		},
-		month = date.toLocaleString("ru", options).split(', ').map(word => word[0].toUpperCase() + word.substring(1)).join(' '),
-		year = date.getFullYear(),
-		hour = date.getHours(),
-		minute = date.getMinutes(),
-		second = date.getSeconds(),
-		hourText = declOfNum(date.getHours(), [ 'час', 'часа', 'часов' ]),
-		minuteText = declOfNum(date.getMinutes(), [ 'минута', 'минуты', 'минут' ]),
-		secondText = declOfNum(date.getSeconds(), [ 'секунда', 'секунды', 'секунд' ]);
-
-      console.log(options);
+   function getColor() {
+      let num = "0123456789abcdef",
+      color = "#";
+      for (var i = 0; i < 6; i++) {
+         color += num[(Math.floor(Math.random() * 16))];
+         document.body.style.background = `${color}`;
+      }
+      console.log(color);
+      colorText.textContent = `${color}`;
+      
+   }
 
 
-	dateOne.innerHTML = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
-	dateTwo.innerHTML = `Сегодня ${month} ${year} года, ${hour} ${hourText}  ${minute} ${minuteText} ${second} ${secondText}`;
-}, 1000);
+   btn.addEventListener('click', function() {
+      getColor();
+   });
