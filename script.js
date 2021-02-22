d7d28a9fcc4ae6f0f83f9821ec22298ae5b409b6
Valentin Venzhega <valek917@yandex.ru>
2021-02-22 17:22:20 +0300

    задание 18-1 выполнено

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 
 'use strict';
 
-const btn = document.querySelector('.btn'),
-   colorText = document.querySelector('.color');
+let print1 = function() {
+	console.log('Крот');
+};
+let print2 = function() {
+	console.log('овце,');
+};
+let print3 = function() {
+	console.log('жирафу,');
+};
+let print4 = function() {
+	console.log('зайке');
+};
+let print5 = function() {
+	console.log('голубые');
+};
+let print6 = function() {
+	console.log('сшил');
+};
+let print7 = function() {
+	console.log('фуфайки');
+};
 
-   function getColor() {
-      let num = "0123456789abcdef",
-      color = "#";
-      for (var i = 0; i < 6; i++) {
-         color += num[(Math.floor(Math.random() * 16))];
-         document.body.style.background = `${color}`;
-      }
-      console.log(color);
-      colorText.textContent = `${color}`;
+let func1 = function() {
+   print3();
+};
+
+let func2 = function() {
+	func1();
+
+	setTimeout(function() {
+      print7();
+
+	}, 1000);
+};
+
+let func3 = function() {
+	setTimeout(function() {
+		func2();
+      print4();
+
+	}, 250);
+   print2();
+};
+
+setTimeout(function() {
+   print5();
+
+	setTimeout(function() {
+      print6();
       
-   }
+	}, 750);
+}, 500);
 
-
-   btn.addEventListener('click', function() {
-      getColor();
-   });
+print1();
+func3();
