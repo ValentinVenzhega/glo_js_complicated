739f3a33d304eef08210cc2727496549789e4f83
Valentin Venzhega <valek917@yandex.ru>
2021-02-22 20:07:27 +0300

    задание 18-3 выполнено

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
 
 'use strict';
+const input = document.querySelector('.input'),
+	text = document.querySelector('.text');
 
-let car = document.querySelector('.car'),
-start = document.querySelector('.start'),
-reset = document.querySelector('.reset'),
-count = 0;
-
-let carInterval;
-let carAnimation = function() {
-	
-	count++;
-	if (count < 100) {
-		carInterval = requestAnimationFrame(carAnimation);
-		car.style.left = count*10 + 'px';
-	} else {
-		start.disabled = true;
-		cancelAnimationFrame(carInterval);
-	}
-	console.log(count);
+const debounce = (fn, ms) => {
+	let timeout;
+	return function() {
+		const fnCall = () => fn.apply(this, arguments);
+		clearTimeout(timeout);
+		timeout = setTimeout(fnCall, ms);
+	};
 };
-let animate = false;
-start.addEventListener('click', function() {
-	if(!animate) {
-		carInterval = requestAnimationFrame(carAnimation);
-		animate = true;
-	} else {
-		cancelAnimationFrame(carInterval);
-		animate = false;
-	}
-	
-});
 
-reset.addEventListener('click', function() {
-	count = 0;
-	car.style.left = count + 'px';
-	carAnimation();
-	cancelAnimationFrame(carInterval);
-	start.disabled = false;
-	animate = false;
-	
-});
+function onChange() {
+	text.innerHTML = input.value;
+}
+
+onChange = debounce(onChange, 300);
+
+input.addEventListener('keyup', onChange);
